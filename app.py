79e59c4ffe172a0851d5e537b9112856ca441c0b
wakoTheDev <wakopaul19@gmail.com>
2025-02-10 15:56:05 +0300

    UPDATES

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def process_id_card():
         logger.error(f"Error processing request: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run()
